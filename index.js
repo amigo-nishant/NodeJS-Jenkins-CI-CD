4d153ec15fcef195ad5a81bc28dbcfae1e28bcfc
Nishant <nishant.nath@tweeny.in>
2022-10-07 12:52:42 +0530

    Added and checked tests

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 app.get('/', (req, res) => {
-    res.send("Hello World"
-});
+    res.send("Hello World");
+})
 app.listen( PORT, () => console.log("Server is listening to port" + PORT ));
