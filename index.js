892701cb9565b3c6ad5b2d7f8d0eab617eb84d21
amigo-nishant <108796288+amigo-nishant@users.noreply.github.com>
2022-10-25 15:41:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World, We are in PROD environment");
 })
 app.listen( PORT, () => console.log("Server is listening to port" + PORT ));
